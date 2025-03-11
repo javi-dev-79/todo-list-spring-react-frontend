642de7c34c51c25d0250b3823291f06935fae3be
javi-dev-79 <javi.dev.79@gmail.com>
2025-03-11 11:46:28 +0000

    chore: install and configure Chakra UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
-import './App.css'
+import { Box, Heading, Text } from '@chakra-ui/react'
 
 function App() {
-  return <></>
+  return (
+    <Box p={5}>
+      <Heading size="xl">Hello Chakra UI</Heading>
+      <Text>This is a simple test component</Text>
+    </Box>
+  )
 }
 
 export default App
